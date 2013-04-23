97885379f95a0e2622903b24ebf8c1c5eddbdf39
Djilali Tabbouche <dt@trade-media.com>
2013-04-24 01:01:26 +0400

    deleted mongodb.sh

--- mongodb.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# add 10gen package GPG key and repository
-apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list
-
-# update apt
-apt-get update 
-apt-get install -y --force-yes mongodb-10gen
